e3eea6724e3b760d5c7a61f8795f7389e4cc2b27
Vamshi Krishna Ananthasetty <vamshikrishna@VamshisLaptop2.lan>
2024-11-23 00:50:51 -0600

    Initial commit with static files

--- src/lib/gtag.ts
+++ src/lib/gtag.ts
@@ -2,9 +2,9 @@ export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_ID
 
 export const pageview = (url: URL) => {
     // @ts-ignore
-    window.gtag("config", GA_TRACKING_ID, {
-        page_path: url
-    });
+    // window.gtag("config", GA_TRACKING_ID, {
+    //     page_path: url
+    // });
 };
 
 type GTagEvent = {
@@ -16,9 +16,9 @@ type GTagEvent = {
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/events
 export const event = ({ action, category, label, value }: GTagEvent) => {
-    window.gtag("event", action, {
-        event_category: category,
-        event_label: label,
-        value: value
-    });
+    // window.gtag("event", action, {
+    //     event_category: category,
+    //     event_label: label,
+    //     value: value
+    // });
 };

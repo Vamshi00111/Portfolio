98d48cd29260c85331fd5995916029a5ca6fd087
Vamshi Krishna <165508436+Vamshi00111@users.noreply.github.com>
2024-11-24 17:46:43 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export", // Enables static export
-    basePath: "/Vamshi00111.github.io", // Remove trailing slash here
-    assetPrefix: "/Vamshi00111.github.io",
+    basePath: "/Portfolio", // Remove trailing slash here
+    assetPrefix: "/Portfolio",
     images: {
         unoptimized: true, // Disable image optimization for static export
     },

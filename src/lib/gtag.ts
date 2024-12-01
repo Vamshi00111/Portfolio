ab07c452fa7566a19cd0bc3fe31d91abd6a2d657
Vamshi Krishna Ananthasetty <vamshikrishna@Vamshis-Laptop-2.local>
2024-11-30 23:03:37 -0600

    Remove gtag

--- src/lib/gtag.ts
+++ src/lib/gtag.ts
@@ -1,5 +1,5 @@
 // export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_ID
-
+export {};
 // export const pageview = (url: URL) => {
 //     // @ts-ignore
 //     window.gtag("config", GA_TRACKING_ID, {
